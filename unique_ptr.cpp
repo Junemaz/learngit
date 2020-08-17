15ca816d90b2744e7a9a52ff0b8e332d7d54027a
Junemaz <1161954459@qq.com>
2020-08-18 00:15:19 +0800

    wrote a simple unique_ptr

---
+++ unique_ptr.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+template <typename T>
+class UniquePointer {
+public:
+	UniquePointer() :ptr_(nullptr) {}
+	UniquePointer(T* ptr) :ptr_(ptr) {}
+
+	UniquePointer(const UniquePointer&&) = delete;
+	UniquePointer(UniquePointer&& other) {
+		if (other.ptr_ != this->ptr_) {
+			ptr_ = other.ptr_;
+			other.ptr_ = nullptr;
+		}
+	}
+
+	UniquePointer& operator=(const UniquePointer&&) = delete;
+	UniquePointer& operator=(UniquePointer&& other) {
+		if (other.ptr_ != this->ptr_) {
+			ptr_ = other.ptr_;
+			other.ptr_ = nullptr;
+		}
+		return *this;
+	}
+
+	operator bool() {
+		return ptr_;
+	}
+
+	T* get() {
+		return ptr_;
+	}
+	T& operator*() {
+		return ptr_;
+	}
+	T& operator->() {
+		return ptr_;
+	}
+	void reset(T* ptr = nullptr) {
+		if (ptr_) {
+			ptr_ = nullptr;
+		}
+		ptr_ = ptr;
+	}
+
+	~UniquePointer() {
+		if (ptr_ != nullptr) {
+			delete ptr_;
+		}
+	}
+private:
+	T* ptr_;
+};
+
+struct Demo {
+	~Demo() {
+		cout << "xigoule" << endl;
+	}
+};
+
+
+int main() {
+	Demo* demo = new Demo();
+	UniquePointer<Demo> ptr(demo);
+	UniquePointer<Demo> ptrs(new Demo());
+	ptr = move(ptr);
+
+	return 0;
+}
